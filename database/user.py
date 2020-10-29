3803bb2eb698fe8360cc0478bde03d9dd3eb6ba5
TimothyWang56 <timothywang56@gmail.com>
2020-10-29 15:20:17 -0400

    add password hash and username/password verification

--- database/user.py
+++ database/user.py
@@ -1,7 +1,8 @@
 # coding=utf-8
 
-from sqlalchemy import Column, String, Integer, Table
+from sqlalchemy import Column, String, Integer, Table, Binary
 from sqlalchemy.orm import relationship, backref
+import bcrypt
 
 from base import Base
 
@@ -11,7 +12,8 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    username = Column(String)
+    username = Column(String, unique=True)
+    passhash = Column(Binary)
 
     leagues = relationship(
         "League",
@@ -20,5 +22,14 @@ class User(Base):
         passive_deletes=True
     )
 
-    def __init__(self, username):
+    def __init__(self, username, password):
         self.username = username
+        password_as_bytes = password.encode('utf-8')
+        self.set_password(password_as_bytes)
+
+    def set_password(self, password):
+        self.passhash = bcrypt.hashpw(password, bcrypt.gensalt())
+
+    def check_password(self, password):
+        password_as_bytes = password.encode('utf-8')
+        return bcrypt.checkpw(password_as_bytes, self.passhash)
