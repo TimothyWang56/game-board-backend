631c45c5db1d24e2d3bdf25e9e23fa4f34f08782
TimothyWang56 <timothywang56@gmail.com>
2020-10-30 11:54:02 -0400

    create basic league route with authenticate decorator

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from app.routes import auth_routes
+from app.routes import auth_routes, league_routes
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(auth_routes.auth_api, url_prefix='/auth')
-
+    app.register_blueprint(league_routes.league_api, url_prefix='/leagues')
     return app
--- app/routes/league_routes.py
+++ app/routes/league_routes.py
@@ -1,12 +1,22 @@
 from flask import Blueprint, request, abort, jsonify, Response
 from functools import wraps
+from app.services.user import authenticate_user
 
 league_api = Blueprint('league_api', __name__)
 
 def authenticate(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
-        #todo - authenticate
+        token = request.headers['Authorization']
+        auth = authenticate_user(token)
+        print("auth: ", auth)
+        if (not auth['authorized']):
+            return Response('Authorization failed', 401)
         return f(*args, **kwargs)
     return wrapper
-    
+
+@league_api.route('/<string:user_id>', methods=['GET'])
+@authenticate
+def getLeagueData(user_id):
+    print(user_id)
+    return jsonify({'hi': 'hi'}), 200
