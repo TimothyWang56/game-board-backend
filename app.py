12aa042b628f36acccf20692b16288a30273c68e
TimothyWang56 <timothywang56@gmail.com>
2020-10-30 10:54:08 -0400

    create template authenticate decorator for routes

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask, request, abort, jsonify
+from flask import Flask, request, abort, jsonify, Response
 from funcs.user_funcs import register_user, login_user
+from functools import wraps
 
 app = Flask(__name__)
+
+def authenticate(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        #todo - authenticate
+        return f(*args, **kwargs)
+    return wrapper
+
 
 @app.route('/register', methods = ['POST'])
 def register():
