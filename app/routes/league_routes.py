83e1c8c3f780d44d9602cc2b12bbb3991259535f
TimothyWang56 <timothywang56@gmail.com>
2020-10-30 12:51:05 -0400

    verify request is from the correct user

--- app/routes/league_routes.py
+++ app/routes/league_routes.py
@@ -13,15 +13,19 @@ def authenticate(f):
         print("auth: ", auth)
         if (not auth['authorized']):
             return Response('Authorization failed', 401)
+        request.user = auth['content']
         return f(*args, **kwargs)
     return wrapper
 
 @league_api.route('/<string:user_id>', methods=['GET'])
 @authenticate
 def get_league_data(user_id):
-    res = league_data(user_id)
-
-    if (not res['success']):
+    # verify request is from the correct user
+    if not (user_id == str(request.user)):
         abort(401)
+
+    res = league_data(user_id)
+    if (not res['success']):
+        abort(500)
 
     return jsonify(res['data']), 200
