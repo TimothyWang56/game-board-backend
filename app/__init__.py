725f63426a663b380f70618e86c5fd4321b2b4cd
TimothyWang56 <timothywang56@gmail.com>
2020-10-30 15:12:47 -0400

    add cors dependency

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from app.routes import auth_routes, league_routes
 
 def create_app():
     app = Flask(__name__)
+    cors = CORS(app)
     app.register_blueprint(auth_routes.auth_api, url_prefix='/auth')
     app.register_blueprint(league_routes.league_api, url_prefix='/leagues')
     return app
