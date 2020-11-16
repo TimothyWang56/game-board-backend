69a6938de389029fb4a5405d9de0ffe37e11f06b
TimothyWang56 <timothywang56@gmail.com>
2020-11-16 17:06:41 -0500

    add players association to game

--- app/models/game.py
+++ app/models/game.py
@@ -5,6 +5,11 @@ from sqlalchemy.orm import relationship
 
 from app.models.base import Base
 
+game_player_association = Table('game_players', Base.metadata,
+    Column('game_id', Integer, ForeignKey('games.id', ondelete="SET NULL")),
+    Column('user_id', Integer, ForeignKey('users.id', ondelete="SET NULL"))
+)
+
 class Game(Base):
     __tablename__ = 'games'
 
@@ -13,6 +18,10 @@ class Game(Base):
     league = relationship("League", back_populates="games")
     date = Column(DateTime)
     winner = Column(Integer, ForeignKey('users.id', ondelete="SET NULL"), nullable=True)
+
+    players = relationship(
+        "User", secondary=game_player_association
+    )
 
     def __init__(self, date, winner=None):
         self.date = date
