cfa19a1d4d42cbe84f04510ab473d3aa22ebba5b
TimothyWang56 <timothywang56@gmail.com>
2020-11-16 15:46:17 -0500

    add support credentials to cors

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from app.routes import auth_routes, league_routes
 
 def create_app():
     app = Flask(__name__)
-    cors = CORS(app)
+    cors = CORS(app, supports_credentials=True)
     app.register_blueprint(auth_routes.auth_api, url_prefix='/auth')
     app.register_blueprint(league_routes.league_api, url_prefix='/leagues')
     return app
